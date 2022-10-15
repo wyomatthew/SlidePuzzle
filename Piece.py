67f273df0ce13b43cc63e3dfe4f2550bda4d1f78
Matthew Pickering <mpick@seas.upenn.edu>
2022-10-15 15:06:42 -0400

    Printing pieces with PIDs

--- Piece.py
+++ Piece.py
@@ -1,4 +1,6 @@
 import click
+from math import floor, ceil
+from typing import Optional
 from dataclasses import dataclass, field
 
 color_map = {
@@ -48,6 +50,7 @@ Y_UNIT = 3 # Height of a (1 x 1) piece in characters
 class Piece(object):
     pos: tuple[int, int]
     dim: tuple[int, int]
+    pid: Optional[int] = None
     col: tuple[int, int, int] = field(default=None, init=False, compare=False)
 
     def __post_init__(self):
@@ -56,13 +59,18 @@ class Piece(object):
         self.col = color_map.get(self.dim, None)
         self.str_list = list()
 
-        char_height = self.dim[0] * 3 # Height of print out of object
-        for i in range(self.dim[0] * 3):
+        char_height = self.dim[0] * Y_UNIT # Height of print out of object
+        for i in range(char_height):
             num_middle = self.dim[1] * 5 - 2 # Number of characters between left and right edges
             if i == 0: # Handle case we are first line
                 line_str = f" {'_' * num_middle} "
             elif i == char_height - 1: # Handle case we are last line
                 line_str = f"|{'_' * num_middle}|"
+            elif i == char_height // 2 and self.pid is not None: # Handle case we are on middle line
+                pid_str = str(self.pid)
+                num_left = floor((num_middle - len(pid_str)) / 2)
+                num_right = ceil((num_middle - len(pid_str)) / 2)
+                line_str = f"|{' ' * (num_left)}{pid_str}{' ' * num_right}|"
             else:
                 line_str = f"|{' ' * num_middle}|"
             self.str_list.append(line_str)
